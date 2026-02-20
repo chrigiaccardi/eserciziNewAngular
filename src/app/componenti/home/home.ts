b094da9ad21821b9af08d53ab49eae2dff5ec7b6
Christian Giaccardi <christianerita@Mac.lan>
2026-02-20 07:49:26 +0100

    Inizio Template Driven Form

--- src/app/componenti/home/home.ts
+++ src/app/componenti/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
