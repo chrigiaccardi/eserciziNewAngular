2af606544952acf70fd1cdc3c447f263f9f9afce
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac-2.lan>
2026-02-20 22:18:00 +0100

    Spiegazione Reactive Form

--- src/app/componenti/home/home.ts
+++ src/app/componenti/home/home.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit{
+  formreactive!: FormGroup
+  
+  ngOnInit(): void {
+    this.formreactive = new FormGroup({
+      nome: new FormControl(null,Validators.required),
+      cognome: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      colore: new FormControl(),
+      })
+  }
+  
 
   onSubmit(form: NgForm) {
     console.log(form)
+  }
+  onSubmitR() {
+    console.log(this.formreactive)
   }
 }
