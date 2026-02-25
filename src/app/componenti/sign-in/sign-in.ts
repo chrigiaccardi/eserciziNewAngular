a09fb6e4e6d1fbbc957c6ff475dcfc5f2715b6ec
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-02-25 14:13:30 +0100

    spiegazione login e logout

--- src/app/componenti/sign-in/sign-in.ts
+++ src/app/componenti/sign-in/sign-in.ts
@@ -14,11 +14,21 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class SignIn {
   constructor(private authService: Auth) { }
+  
     onSubmit(form: NgForm) {
     const email = form.value.email
     const password = form.value.password
     console.log(email, password)
-    this.authService.signIn(email, password).subscribe(data => { console.log(data) })
+      this.authService.signIn(email, password).subscribe((data: any) => {
+        console.log(data)
+
+        const expirationDate = new Date(new Date().getTime() + data.expiresIn *1000)
+        this.authService.createUser(data.email, data.localId, data.idToken, expirationDate)
+        localStorage.setItem('user', JSON.stringify(this.authService.user))
+        
+        console.log(this.authService.user)
+      
+     });
     form.reset()
   }
 }
--- src/app/service/auth/auth.ts
+++ src/app/service/auth/auth.ts
@@ -1,23 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../../models/user.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   constructor(private http: HttpClient) { }
-  APIKEY= '1'
-  signInUrl: string = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.APIKEY}`
-  signUpUrl: string = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.APIKEY}`
-  isLoggedIn: boolean = true
+  APIKEY = 'apifirebase';
+  signInUrl: string = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.APIKEY}`;
+  signUpUrl: string = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.APIKEY}`;
+  isLoggedIn: boolean = true;
+  user!: User
+
   isAuthenticated():boolean {
     return this.isLoggedIn
+  };
+
+  createUser(email: string, id: string, token: string, expirationDate: Date) {
+    this.user = new User(email, id, token, expirationDate)
   }
 
   signUp(email: string, password:string) {
     return this.http.post(this.signUpUrl,{email: email, password: password, returnSecureToken: true})
-  }
+  };
+
   signIn(email: string, password:string) {
     return this.http.post(this.signInUrl,{email: email, password: password, returnSecureToken: true})
-  }
+  };
 }
